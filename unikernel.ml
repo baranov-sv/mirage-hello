1fccd7889e91c326cc73a14584cb4c302498471b
Pavel Argentov <argentoff@gmail.com>
2017-03-14 01:31:55 +0300

    Multiple loops running concurrently

--- unikernel.ml
+++ unikernel.ml
@@ -1,10 +1,14 @@
 module Hello (Time : Mirage_time_lwt.S) = struct
+  let durations = [1; 2; 3; 4; 5]
+  and iterations = 1000
+
   let start _time =
-    let rec loop = function
+    let rec loop count n =
+      match count with
       | 0 -> Lwt.return_unit
-      | n ->
-         Logs.info (fun f -> f "hello");
-         Time.sleep_ns (Duration.of_sec 5) >> loop (n - 1)
+      | c ->
+         Logs.info (fun f -> f "hello from instance %i iteration %i" n (iterations - c));
+         Time.sleep_ns (Duration.of_sec n) >> loop (c - 1) n
     in
-    loop 1000
+    Lwt.join @@ ListLabels.map ~f:(loop iterations) durations
 end
